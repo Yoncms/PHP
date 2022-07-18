6f3f7f27cca5ee55a05ad138671e205be36f8b9b
Yoncms <106565579+Yoncms@users.noreply.github.com>
2022-07-18 09:56:31 +0800

    Add files via upload

---
+++ constrol/index.php
@@ -0,0 +1,31 @@
+<?php
+namespace yoncms\index;
+
+use yoncms\model\model;
+
+class index extends model{
+
+  public function show(){
+
+    $this->fetch();
+
+  }
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
